01d2ee3aad89a25f1ebbf475890c0de61335723b
tiff-wang <wangtiffany@gmail.com>
2019-03-17 03:04:36 -0400

    renamed test_image to predictor, and made it OOP

--- predictor.py
+++ predictor.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 import torchvision.transforms.functional as TF
 
-from main import predict
+from model.model import MNISTModel
 from PIL import Image
 
 size = (28, 28)
@@ -19,7 +19,6 @@ def preprocess_img(image):
 		image = image.convert('L')
 	image = image.resize(size)
 	return image
-	
 
 def image_loader(image):
 	''' 
@@ -29,25 +28,20 @@ def image_loader(image):
 	image = image.unsqueeze(0)
 	return image
 
-def test_single_image(image_path): 
-	'''
-	This method tests the model using a single image. 
-	'''
-	test_img = Image.open(image_path)
-	
-	# First put the image into the right format. 
-	sample = preprocess_img(test_img)
-	sample = image_loader(sample)
+class Predictor: 
+	def __init__(self):
+		self.model = MNISTModel()
+		self.model.net.load_state_dict(torch.load('model/results/model.pth'))
 
-	# Predict the output. 
-	prediction_tensor = predict(sample)
-	prediction_label = torch.argmax(prediction_tensor)
-	print('The model recognizes this picture as a {}'.format(prediction_label))
-	
-	# Show the test image. 
-	plt.imshow(test_img)
-	plt.show()
-
-if __name__ == '__main__' :
-    test_single_image('./test.jpg')
+	def predict(self, request):
+		'''
+		This method reads the file uploaded from the Flask application POST request, 
+		and performs a prediction using the MNIST model. 
+		'''
+		f = request.files['image']
+		image = Image.open(f)
+		image = preprocess_img(image)
+		image = image_loader(image)
+		prediction = self.model.net(image)
+		return prediction
 	
