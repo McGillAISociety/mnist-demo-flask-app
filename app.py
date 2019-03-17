9c4bf6eba930f2ea9d7438e6f215f33500219626
tiff-wang <wangtiffany@gmail.com>
2019-03-17 03:04:47 -0400

    frontend and backend connected

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from PIL import Image
+from predictor import Predictor
+
+import numpy as np
+import torch
+
+# Initialize the predictor object.
+predictor = Predictor()
+
+# Initialize Flask app.
+app = Flask(__name__)
+
+# Base endpoint to perform prediction 
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        prediction = predictor.predict(request)
+        prediction = torch.argmax(prediction)
+        return render_template('index.html', prediction=prediction)
+    else:
+        return render_template('index.html', prediction=None)
+		
+if __name__ == '__main__':
+   app.run()
