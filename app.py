7acb5b56292c8a1622204e148e8b51292f39c4a3
tiff-wang <wangtiffany@gmail.com>
2019-03-18 16:49:38 -0400

    demo incomplete code

--- app.py
+++ app.py
@@ -6,20 +6,7 @@ from predictor import Predictor
 import numpy as np
 import torch
 
-# Initialize the predictor object.
-predictor = Predictor()
-
-# Initialize Flask app.
-app = Flask(__name__)
-
-# Base endpoint to perform prediction.
-@app.route('/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        prediction = predictor.predict(request)
-        return render_template('index.html', prediction=prediction)
-    else:
-        return render_template('index.html', prediction=None)
+# ======== YOUR CODE ========= #
 		
 if __name__ == '__main__':
    app.run()
